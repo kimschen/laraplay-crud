4e2b96ce8372d479b1c7a59389569497f32f3d39
kimsphere <kimiley90@gmail.com>
2017-08-18 15:58:53 +0800

    [LRP4] CRUD practice - Added master & create page, routing, controller, list table and list model.

---
+++ resources/views/list/create.blade.php
@@ -0,0 +1,27 @@
+@extends('master')
+@section('content')
+<div class="container">
+	<form>
+		<div class="form-group row">
+			<label for="lgFormGroupInput" class="col-sm-2 col-form-label col-form-label-lg">
+				Title
+			</label>
+			<div>
+				<input type="text" class="form-control form-control-lg" id="lgFormGroupInput" placeholder="title" name="title">
+			</div>
+		</div>
+		<div>
+			<label for="smFormGroupInput" class="col-sm-2 col-form-label col-form-label-sm">
+				Post
+			</label>
+			<div>
+				<textarea name="post" rows="8" cols="80" length="10"></textarea>
+			</div>
+		</div>
+		<div class="form-group row">
+			<div class="col-md-2"></div>
+			<input type="submit" class="btn btn-primary">
+		</div>
+	</form>
+</div>
+@endsection
---
+++ resources/views/master.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>CRUD Lists</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <h1>Hello CRUD list</h1>
+        <br><br>
+        <!-- yield the content from any other extension blade file where has @extend('master') & @section('content') -->
+        @yield('content')
+    </body>
+</html>
